821be4e7a2c796e5f1677025887790dff8e9df67
sh9336 <saurabh25decem2020@gmail.com>
2023-10-01 23:59:14 +0530

    added profile me custom endpoints

---
+++ src/api/profile/controllers/profile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * profile controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::profile.profile',({Strapi})=>({
+    async createMe(ctx) {
+        try{
+           const user= ctx.state.user;
+           if(!user){
+            return ctx.badRequest(401,[{messages: "No authorized user found"}]);
+           }
+           const result = await strapi.entityService.create('api::profile.profile',{
+            data: {
+                fullName: ctx.request.body.fullName,
+                email: user.email,
+                user: user.id
+            }
+           });
+           return result;
+        } catch (err){
+            return ctx.badRequest(500,[{messages:[{id:'Error'}]}]);
+        }
+    },
+    async getMe(ctx) {
+        try{
+           const user= ctx.state.user;
+           if(!user){
+            return ctx.badRequest(401,[{messages: "No authorized user found"}]);
+           }
+            const result = await strapi.db.query('api::profile.profile').findOne({
+             where:{
+                user: {
+                    id: {
+                        $eq: user.id
+                    }
+                }
+             },
+             populate: {
+                
+                // @ts-ignore
+                image: true,
+             },
+           });
+           return result;
+        } catch (err){
+            return ctx.badRequest(500,[{messages:[{id:'Error'}]}]);
+        }
+    }
+}));
